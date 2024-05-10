d535b9e25de97590c5120e0287d7589e7e2596ca
BossBaby25 <meraj15-4213@diu.edu.bd>
2024-05-11 01:34:11 +0600

    final code
    e-commerce mern stack

---
+++ config/db.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+import  colors  from 'colors';
+const connectDB = async ()=>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`connect to mongodb ${conn.connection.host}`.bgMagenta.white);
+    } catch (error) {
+        console.log(`error in mongobd ${error}`.bgRed.white);
+    }
+};
+export default connectDB;
